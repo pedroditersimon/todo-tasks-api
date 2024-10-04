d068321a0162af2a1f0b4e0fb18b45749c7d3c5e
pedroditersimon <114523969+pedroditersimon@users.noreply.github.com>
2024-10-04 10:30:33 -0300

    AddGoalIsCompleted migration

---
+++ TodoAPI.API/Migrations/20241004132952_AddGoalIsCompleted.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoAPI.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGoalIsCompleted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompleted",
+                table: "TodoGoal",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "TodoGoal",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "IsCompleted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "TodoGoal",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "IsCompleted",
+                value: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                table: "TodoGoal");
+        }
+    }
+}
