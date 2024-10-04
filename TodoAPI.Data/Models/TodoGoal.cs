405802eaedc9dca0c3d75d469189dd3bda2e7aec
pedroditersimon <114523969+pedroditersimon@users.noreply.github.com>
2024-10-04 09:24:32 -0300

    Added fav propierty

--- TodoAPI.Data/Models/TodoGoal.cs
+++ TodoAPI.Data/Models/TodoGoal.cs
@@ -2,8 +2,9 @@
 
 public class TodoGoal : EntityBaseModel<int>
 {
-    public string? Name { get; set; }
-    public string? Description { get; set; }
+	public string? Name { get; set; }
+	public string? Description { get; set; }
+	public bool IsFavorite { get; set; }
 
-    public virtual ICollection<TodoTask> Tasks { get; set; }
+	public virtual ICollection<TodoTask> Tasks { get; set; }
 }
--- TodoAPI.Data/Models/TodoTask.cs
+++ TodoAPI.Data/Models/TodoTask.cs
@@ -3,9 +3,9 @@
 public class TodoTask : EntityBaseModel<int>
 {
 
-    public string? Name { get; set; }
-    public string? Description { get; set; }
+	public string? Name { get; set; }
+	public string? Description { get; set; }
 
-    public bool IsCompleted { get; set; }
-
+	public bool IsCompleted { get; set; }
+	public bool IsFavorite { get; set; }
 }
