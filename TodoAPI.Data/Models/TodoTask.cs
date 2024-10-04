2f471a0b762cc4729a55ac370c6d27150e0bd3e7
pedroditersimon <114523969+pedroditersimon@users.noreply.github.com>
2024-10-04 11:35:09 -0300

    added goalID prop and navigator to task

--- TodoAPI.Data/Models/TodoTask.cs
+++ TodoAPI.Data/Models/TodoTask.cs
@@ -1,4 +1,7 @@
-﻿namespace TodoAPI.Data.Models;
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TodoAPI.Data.Models;
 
 public class TodoTask : EntityBaseModel<int>
 {
@@ -8,4 +11,10 @@ public class TodoTask : EntityBaseModel<int>
 
 	public bool IsCompleted { get; set; }
 	public bool IsFavorite { get; set; }
+
+
+	[Column("TodoGoalID")]
+	public int? GoalID { get; set; }
+	[ForeignKey("GoalID")]
+	public TodoGoal? Goal { get; set; }
 }
