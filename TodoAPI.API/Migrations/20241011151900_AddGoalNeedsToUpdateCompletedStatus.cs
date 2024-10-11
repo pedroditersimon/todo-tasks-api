aa6584d84b5f64abbe63574874397c4c3a5d1cfc
pedroditersimon <114523969+pedroditersimon@users.noreply.github.com>
2024-10-11 17:37:17 -0300

    AddGoalNeedsToUpdateCompletedStatus

---
+++ TodoAPI.API/Migrations/20241011151900_AddGoalNeedsToUpdateCompletedStatus.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoAPI.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGoalNeedsToUpdateCompletedStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "NeedsToUpdateCompletedStatus",
+                table: "TodoGoal",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "TodoGoal",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "NeedsToUpdateCompletedStatus",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "TodoGoal",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "NeedsToUpdateCompletedStatus",
+                value: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NeedsToUpdateCompletedStatus",
+                table: "TodoGoal");
+        }
+    }
+}
